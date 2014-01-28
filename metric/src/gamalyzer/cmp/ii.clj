f8a87634b566ba154915dcc2f5d05a18faf58b63
Joseph Osborn <jcosborn@ucsc.edu>
2014-01-27 21:25:30 -0800

    Mostly dummied-out attempt at typing cmp.ii

--- metric/src/gamalyzer/cmp/ii.clj
+++ metric/src/gamalyzer/cmp/ii.clj
@@ -1,9 +1,10 @@
 (ns gamalyzer.cmp.ii
   (:require [gamalyzer.data.input :refer [parts make-domains make-input expand-domain get-domains get-domain]]
-            [gamalyzer.data.range :refer [rrange]]
+            [gamalyzer.data.range :refer [rrange range?]]
             [gamalyzer.read.edn :refer [read-logs]]
             [clojure.math.numeric-tower :refer [abs]]
-            [clojure.test :refer [is]]))
+            [clojure.test :refer [is]]
+						[clojure.core.typed :refer [ann ann-form ann-record def-alias inst loop>]]))
 
 ;;;; takes game traces as input:
 ;;;; {:traces ts :doms doms}
@@ -16,48 +17,65 @@
 
 (declare vs-diss)
 
+(ann ^:no-check vs-diss-vecs [(clojure.lang.IPersistentVector gamalyzer.data.input/ITerm)
+									 (clojure.lang.IPersistentVector gamalyzer.data.input/ITerm)
+									 gamalyzer.data.input/DomainPath
+									 gamalyzer.data.input.Domains -> double])
 (defn- vs-diss-vecs ^double [vs1 vs2 path doms]
-  (let [l1 (double (count vs1))
-       ; l2 (count vs2)
-        each-diss (double (/ 1.0 l1))]
-   ; (when-not (= l1 l2) (throw (IllegalArgumentException. "Sequences of unequal length")))
-    (loop [i (int 0) d (double 0.0)]
-      (if (< i l1)
-        (let [v1 (vs1 i)
-              v2 (vs2 i)
-              new-d (double (* (vs-diss v1 v2 (conj path i) doms) each-diss))]
-          (recur (inc i) (+ d new-d)))
-        d))))
+	(let [l1 (double (count vs1))
+				; l2 (count vs2)
+				each-diss (double (/ 1.0 l1))]
+		(loop> [i :- int, (int 0)
+						d :- double, (double 0.0)]
+					 (if (< i l1)
+						 (let [v1 (vs1 i)
+									 v2 (vs2 i)
+									 new-d (double (* (vs-diss v1 v2 (conj path i) doms) each-diss))]
+							 (recur (inc i) (+ d new-d)))
+						 d))))
 
+(ann ^:no-check vs-diss-lists [(clojure.lang.ISeq gamalyzer.data.input/ITerm)
+										(clojure.lang.ISeq gamalyzer.data.input/ITerm)
+										gamalyzer.data.input/DomainPath
+										gamalyzer.data.input.Domains -> double])
 (defn- vs-diss-lists ^double [vs1 vs2 path doms]
-  (let [l1 (double (count vs1))
-        denom (/ (* l1 (inc l1)) 2)
-        one (double 1.0)]
-    (loop [i (int 0)
-           ii (double l1)
-           d (double 0.0)
-           vs1r vs1
-           vs2r vs2]
-      (if (empty? vs1r)
-        d
-        (let [v1 (first vs1r)
-              v2 (first vs2r)
-              this-diss (double (/ ii denom))
-              new-d (* (vs-diss v1 v2 (conj path i) doms) this-diss)]
-          (recur (inc i)
-                 (- ii one)
-                 (+ d new-d)
-                 (rest vs1r)
-                 (rest vs2r)))))))
+	(let [l1 (double (count vs1))
+				denom (/ (* l1 (inc l1)) 2)
+				one (double 1.0)]
+		(loop> [i :- int, (int 0)
+						ii :- double, (double l1)
+						d :- double, (double 0.0)
+						vs1r :- (clojure.lang.ISeq gamalyzer.data.input/ITerm), vs1
+						vs2r :- (clojure.lang.ISeq gamalyzer.data.input/ITerm), vs2]
+					 (if (empty? vs1r)
+						 d
+						 (let [v1 (first vs1r)
+									 v2 (first vs2r)
+									 this-diss (double (/ ii denom))
+									 new-d (* (vs-diss v1 v2 (conj path i) doms) this-diss)]
+							 (recur (inc i)
+											(- ii one)
+											(+ d new-d)
+											(rest vs1r)
+											(rest vs2r)))))))
 
+(ann ^:no-check vs-diss-nums [double double
+									 gamalyzer.data.input/DomainPath
+									 gamalyzer.data.input.Domains -> double])
 (defn- vs-diss-nums ^double [v1 v2 path doms]
   (let [r (get-domain doms path)
+				_ (assert (range? r))
         rng (double (rrange r))
         d (double (abs (- v1 v2)))]
     (if (== d 0.0) 0.0 (/ d rng))))
 
+(ann vs-diss-prim [clojure.lang.Keyword clojure.lang.Keyword -> double])
 (defn- vs-diss-prim ^double [v1 v2] (if (= v1 v2) 0.0 1.0))
 
+(ann ^:no-check vs-diss [gamalyzer.data.input/ITerm
+							gamalyzer.data.input/ITerm
+							gamalyzer.data.input/DomainPath
+							gamalyzer.data.input.Domains -> double])
 (defn- vs-diss ^double [vs1 vs2 path doms]
   (cond
    (= vs1 vs2) 0.0
@@ -65,14 +83,17 @@
    (and (sequential? vs1) (sequential? vs2)) (vs-diss-lists vs1 vs2 path doms)
    (and (number? vs1) (number? vs2)) (vs-diss-nums vs1 vs2 path doms)
    (and (not (sequential? vs1)) (not (sequential? vs2))) (vs-diss-prim vs1 vs2)
-   true (throw (Exception. (str "Could not find distance between " (.toString vs1) " and " (.toString vs2))))))
+   true (throw (Exception. (str "Could not find distance between " vs1 " and " vs2)))))
 
+(ann ^:no-check distance [gamalyzer.data.input.Input
+							 gamalyzer.data.input.Input
+							 gamalyzer.data.input.Domains -> double])
 (defn distance ^double [i1 i2 doms]
   (let [[d1 vs1] (parts i1)
         [d2 vs2] (parts i2)]
     (if-not (= d1 d2)
-			1.0
-      (* (vs-diss vs1 vs2 [] (get-domains doms d1)) 0.8))))
+			(double 1.0)
+      (double (* (vs-diss vs1 vs2 [] (get-domains doms d1)) 0.8)))))
 
 ; Informal tests and usage examples.
 
